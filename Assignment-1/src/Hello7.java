6548b4f7ba78cecffa33aeea410c619d8524de99
ASUS <ASUS@LAPTOP-AOM7QAGL>
2022-06-16 20:19:09 +0530

    Commit1

---
+++ Assignment-1/src/Hello7.java
@@ -0,0 +1,17 @@
+
+public class Hello7 {
+	public static void main(String[] args) 
+	{	
+         for(int i=1; i<=10; i++)
+         {
+        	 if(i == 1)
+        	 {
+        		 continue;   // break;
+        	 }
+        	 System.out.println(i);
+         }
+	}
+
+
+
+}
